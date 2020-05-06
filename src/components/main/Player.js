cf5aa962a4e7fb7513770ab35248224e3d009042
developer <developer@developer.com>
2020-05-06 18:24:53 +0530

    Player Done

--- src/components/main/Player.js
+++ src/components/main/Player.js
@@ -136,195 +136,236 @@ export default function Player(props) {
 
             <SafeAreaView style={{ flex: 1, }}>
 
-                <View style={{
-                    marginHorizontal: normalise(15),
-                    width: normalise(290),
-                    marginTop: normalise(15),
-                    flexDirection: 'row', alignItems: 'center',
-                    justifyContent: 'space-between'
-                }}>
-
-
-                    <Image source={ImagePath.dp1}
-                        style={{ height: normalise(24), width: normalise(24) }}
-                        resizeMode="contain" />
-
-
+                <ScrollView>
 
                     <View style={{
-                        flexDirection: 'column', alignItems: 'flex-start', width: '50%',
-                        marginRight: normalise(30)
+                        marginHorizontal: normalise(15),
+                        width: normalise(290),
+                        marginTop: normalise(15),
+                        flexDirection: 'row', alignItems: 'center',
+                        justifyContent: 'space-between'
                     }}>
 
-                        <Text style={{
-                            color: Colors.grey, fontSize: normalise(10),
-                            fontWeight: 'bold',
-                        }} numberOfLines={1}> Posted By </Text>
 
-                        <Text style={{
-                            color: Colors.white, fontSize: normalise(11),
-                            fontWeight: 'bold',
-
-                        }} numberOfLines={1}> ShimShimmer </Text>
+                        <Image source={ImagePath.dp1}
+                            style={{ height: normalise(24), width: normalise(24) }}
+                            resizeMode="contain" />
 
 
+
+                        <View style={{
+                            flexDirection: 'column', alignItems: 'flex-start', width: '50%',
+                            marginRight: normalise(30)
+                        }}>
+
+                            <Text style={{
+                                color: Colors.grey, fontSize: normalise(10),
+                                fontWeight: 'bold',
+                            }} numberOfLines={1}> Posted By </Text>
+
+                            <Text style={{
+                                color: Colors.white, fontSize: normalise(11),
+                                fontWeight: 'bold',
+
+                            }} numberOfLines={1}> ShimShimmer </Text>
+
+
+                        </View>
+
+
+
+
+
+                        <View style={{
+                            height: normalise(40), width: normalise(50), backgroundColor: Colors.black,
+                            justifyContent: 'center',
+                        }}>
+
+                            <TouchableOpacity style={{
+                                height: normalise(25), width: normalise(45),
+                                borderRadius: normalise(5), alignSelf: 'center', backgroundColor: Colors.fadeblack,
+                                justifyContent: 'center', alignItems: 'center'
+                            }} >
+
+                                <Image source={ImagePath.threedots} style={{ height: normalise(15), width: normalise(15) }}
+                                    resizeMode='contain' />
+
+                            </TouchableOpacity>
+                        </View>
                     </View>
 
+                    <TouchableOpacity
+                        style={{
+                            marginTop: normalise(5),
+                            height: normalise(265), width: normalise(290), alignSelf: 'center',
+                            borderRadius: normalise(25), backgroundColor: Colors.darkerblack, borderWidth: normalise(0.5),
+                            borderColor: Colors.grey, shadowColor: "#000", shadowOffset: { width: 0, height: 5, }, shadowOpacity: 0.36,
+                            shadowRadius: 6.68, elevation: 11, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'
+                        }}   >
+
+
+                        <Image source={ImagePath.profiletrack4}
+                            style={{ height: normalise(265), width: normalise(290), borderRadius: normalise(15) }}
+                            resizeMode="cover" />
 
 
 
+                    </TouchableOpacity>
 
-                    <View style={{
-                        height: normalise(40), width: normalise(50), backgroundColor: Colors.black,
-                        justifyContent: 'center'
-                    }}>
+                    <Text style={{
+                        margin: normalise(20),
+                        color: Colors.white, fontSize: normalise(12),
+                        fontWeight: 'bold',
 
-                        <TouchableOpacity style={{
-                            height: normalise(25), width: normalise(45),
-                            borderRadius: normalise(5), alignSelf: 'center', backgroundColor: Colors.fadeblack,
-                            justifyContent: 'center', alignItems: 'center'
-                        }} onPress={() => { props.onPressSecondImage() }} >
-
-                            <Image source={ImagePath.threedots} style={{ height: normalise(15), width: normalise(15) }}
-                                resizeMode='contain' />
-
-                        </TouchableOpacity>
-                    </View>
-                </View>
-
-                <TouchableOpacity
-                    style={{
-                        height: normalise(265), width: normalise(290), alignSelf: 'center',
-                        borderRadius: normalise(25), backgroundColor: Colors.darkerblack, borderWidth: normalise(0.5),
-                        borderColor: Colors.grey, shadowColor: "#000", shadowOffset: { width: 0, height: 5, }, shadowOpacity: 0.36,
-                        shadowRadius: 6.68, elevation: 11, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'
-                    }}   >
-
-
-                    <Image source={ImagePath.profiletrack4}
-                        style={{ height: normalise(265), width: normalise(290), borderRadius: normalise(5) }}
-                        resizeMode="cover" />
-
-
-
-                </TouchableOpacity>
-
-                <Text style={{
-                    margin: normalise(20),
-                    color: Colors.white, fontSize: normalise(12),
-                    fontWeight: 'bold',
-
-                }} numberOfLines={2}>
-                    Played-A-Live (The Bongo {"\n"} Song)
+                    }} numberOfLines={2}>
+                        Played-A-Live (The Bongo {"\n"} Song)
                 </Text>
 
-                <View style={{
-                    flexDirection: 'row', marginLeft: normalise(22), width: normalise(276),
-                    justifyContent: 'space-between'
-                }}>
-                    <Text style={{ color: 'white' }}>
-                        01:30
+                    <View style={{
+                        flexDirection: 'row', marginLeft: normalise(22), width: normalise(276),
+                        justifyContent: 'space-between'
+                    }}>
+                        <Text style={{ color: 'white' }}>
+                            01:30
                     </Text>
-                    <Text style={{ color: 'white' }}>
-                        -2:19
+                        <Text style={{ color: 'white' }}>
+                            -2:19
                         </Text>
-                </View>
-                <Slider
-                    style={{ width: normalise(276), height: 40, marginLeft: normalise(22) }}
-                    minimumValue={0}
-                    maximumValue={1}
-                    minimumTrackTintColor="#FFFFFF"
-                    maximumTrackTintColor="#000000"
-                />
+                    </View>
+                    <Slider
+                        style={{ width: normalise(276), height: 40, marginLeft: normalise(22) }}
+                        minimumValue={0}
+                        maximumValue={1}
+                        minimumTrackTintColor="#FFFFFF"
+                        maximumTrackTintColor="#000000"
+                    />
 
-                <View style={{
-                    flexDirection: 'row', marginLeft: normalise(22), width: normalise(276),
-                    justifyContent: 'space-around', marginTop: normalise(25), alignItems: 'center'
-                }}>
-                    <Image source={ImagePath.backwardicon}
-                        style={{ height: normalise(18), width: normalise(18) }}
-                        resizeMode="contain" />
-                    <TouchableOpacity
-                        onPress={() => {
-                            playing()
-                        }}
-                        style={{
-                            height: normalise(60), width: normalise(60), alignItems: 'center', justifyContent: 'center',
-                            backgroundColor: Colors.white, borderRadius: normalise(30)
+                    <View style={{
+                        flexDirection: 'row', marginLeft: normalise(22), width: normalise(276),
+                        justifyContent: 'space-around', marginTop: normalise(25), alignItems: 'center'
+                    }}>
+                        <Image source={ImagePath.backwardicon}
+                            style={{ height: normalise(18), width: normalise(18) }}
+                            resizeMode="contain" />
+                        <TouchableOpacity
+                            onPress={() => {
+                                playing()
+                            }}
+                            style={{
+                                height: normalise(60), width: normalise(60), alignItems: 'center', justifyContent: 'center',
+                                backgroundColor: Colors.white, borderRadius: normalise(30)
+                            }}>
+                            {playVisible == true ?
+                                <Image source={ImagePath.pauseicon}
+                                    style={{ height: normalise(20), width: normalise(20) }}
+                                    resizeMode="contain" />
+                                :
+
+                                <Image source={ImagePath.playicon}
+                                    style={{ height: normalise(20), width: normalise(20) }}
+                                    resizeMode="contain" />
+
+                            }
+
+                        </TouchableOpacity>
+                        <Image source={ImagePath.forwardicon}
+                            style={{ height: normalise(18), width: normalise(18) }}
+                            resizeMode="contain" />
+                    </View>
+
+
+                    <View style={{
+                        flexDirection: 'row', marginLeft: normalise(15), width: normalise(290),
+                        justifyContent: 'space-around', marginTop: normalise(25), alignItems: 'center'
+                    }}>
+                        <TouchableOpacity style={{
+                            height: normalise(40), width: normalise(42), alignItems: 'center', justifyContent: 'center',
+                            backgroundColor: Colors.fadeblack, borderRadius: normalise(5)
                         }}>
-                        {playVisible == true ?
-                            <Image source={ImagePath.pauseicon}
-                                style={{ height: normalise(20), width: normalise(20) }}
-                                resizeMode="contain" />
-                            :
-
-                            <Image source={ImagePath.playicon}
-                                style={{ height: normalise(20), width: normalise(20) }}
-                                resizeMode="contain" />
-
-                        }
-
-                    </TouchableOpacity>
-                    <Image source={ImagePath.forwardicon}
-                        style={{ height: normalise(18), width: normalise(18) }}
-                        resizeMode="contain" />
-                </View>
-
-
-                <View style={{
-                    flexDirection: 'row', marginLeft: normalise(15), width: normalise(290),
-                    justifyContent: 'space-around', marginTop: normalise(25), alignItems: 'center'
-                }}>
-                    <TouchableOpacity style={{
-                        height: normalise(40), width: normalise(42), alignItems: 'center', justifyContent: 'center',
-                        backgroundColor: Colors.fadeblack, borderRadius: normalise(5)
-                    }}>
-                     <Image source={ImagePath.reactionicon}
+                            <Image source={ImagePath.reactionicon}
                                 style={{ height: normalise(20), width: normalise(20) }}
                                 resizeMode="contain" />
 
 
-                    </TouchableOpacity>
-                    <TouchableOpacity style={{
-                        height: normalise(40), width: normalise(42), alignItems: 'center', justifyContent: 'center',
-                        backgroundColor: Colors.fadeblack, borderRadius: normalise(5)
-                    }}>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{
+                            height: normalise(40), width: normalise(42), alignItems: 'center', justifyContent: 'center',
+                            backgroundColor: Colors.fadeblack, borderRadius: normalise(5)
+                        }}>
 
-<Image source={ImagePath.boxicon}
+                            <Image source={ImagePath.boxicon}
                                 style={{ height: normalise(20), width: normalise(20) }}
                                 resizeMode="contain" />
 
 
-                    </TouchableOpacity>
-                    <TouchableOpacity style={{
-                        height: normalise(40), width: normalise(42), alignItems: 'center', justifyContent: 'center',
-                        backgroundColor: Colors.fadeblack, borderRadius: normalise(5)
-                    }}>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{
+                            height: normalise(40), width: normalise(42), alignItems: 'center', justifyContent: 'center',
+                            backgroundColor: Colors.fadeblack, borderRadius: normalise(5)
+                        }}>
 
-<Image source={ImagePath.sendicon}
+                            <Image source={ImagePath.sendicon}
                                 style={{ height: normalise(20), width: normalise(20) }}
                                 resizeMode="contain" />
 
 
-                    </TouchableOpacity>
-                    <TouchableOpacity style={{
-                        flexDirection:'row',
-                        height: normalise(40), width: normalise(115), alignItems: 'center', justifyContent: 'center',
-                        backgroundColor: Colors.fadeblack, borderRadius: normalise(10)
-                    }}>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{
+                            flexDirection: 'row',
+                            height: normalise(40), width: normalise(115), alignItems: 'center', justifyContent: 'center',
+                            backgroundColor: Colors.fadeblack, borderRadius: normalise(10)
+                        }}>
 
 
-                    
-                         <Image source={ImagePath.commenticon}
+
+                            <Image source={ImagePath.commenticon}
                                 style={{ height: normalise(16), width: normalise(16) }}
                                 resizeMode="contain" />
 
-                     <Text style={{fontSize:normalise(9),color:Colors.white,marginLeft:normalise(5)}}>
-                        3 Comments
+                            <Text style={{ fontSize: normalise(9), color: Colors.white, marginLeft: normalise(5) }}>
+                                3 Comments
                      </Text>
 
-                    </TouchableOpacity>
-                </View>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={{
+                        width: '90%', alignSelf: 'center', backgroundColor: Colors.white,
+                        height: normalise(50), marginTop: normalise(20), alignItems: 'center',
+                        justifyContent: 'center',
+                        borderRadius: normalise(10), marginBottom: normalise(20),
+                    }}>
+
+                        <View style={{
+                            width: '90%', alignSelf: 'center', flexDirection: 'row',
+                            justifyContent: 'space-between', alignItems: 'center',
+                        }}>
+
+                            <TouchableOpacity>
+                                <Image source={ImagePath.playicon}
+                                    style={{
+                                        height: normalise(12), width: normalise(12),
+                                        transform: [{ rotate: '90deg' }]
+                                    }} resizeMode='contain' />
+                            </TouchableOpacity>
+
+                            <Text style={{
+                                color: Colors.darkerblack, fontSize: normalise(12),
+                                fontWeight: '500'
+                            }}>Minimize Player</Text>
+
+                            <TouchableOpacity>
+                                <Image source={ImagePath.playicon}
+                                    style={{
+                                        height: normalise(12), width: normalise(12),
+                                        transform: [{ rotate: '90deg' }]
+                                    }} resizeMode='contain' />
+                            </TouchableOpacity>
+
+                        </View>
+                    </View>
+
+                </ScrollView>
             </SafeAreaView>
         </View>
     )
