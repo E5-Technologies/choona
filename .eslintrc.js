a2001326a02a7cfb1bef771bd09d777de18eeb14
Daniel Vernon <danpvernon@gmail.com>
2021-07-08 18:05:26 +0100

    fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,4 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-  plugins: ['unused-imports'],
-  rules: {
-    'react-native/no-inline-styles': 0,
-    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-      },
-    ],
-  },
 };
