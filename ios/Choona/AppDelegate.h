818d62144e5d511ab85386045a00d2497ee8310b
Jayant Sharma <jayantsharma534@gmail.com>
2025-04-15 13:30:18 +0530

    iOS Setup

--- ios/Choona/AppDelegate.h
+++ ios/Choona/AppDelegate.h
@@ -1,11 +1,15 @@
 #import <React/RCTBridgeDelegate.h>
+#import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
+#import <React/RCTLinkingManager.h>
 #import "RNAppAuthAuthorizationFlowManager.h"
 
+// @interface AppDelegate : RCTAppDelegate
+
+//@interface AppDelegate : RCTAppDelegate <RNAppAuthAuthorizationFlowManager>
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, RNAppAuthAuthorizationFlowManager>
 
-
 @property (nonatomic, strong) UIWindow *window;
-@property(nonatomic, weak)id<RNAppAuthAuthorizationFlowManagerDelegate>authorizationFlowManagerDelegate;
+@property(nonatomic, weak) id<RNAppAuthAuthorizationFlowManagerDelegate> authorizationFlowManagerDelegate;
 
 @end
